d54324c4cff1ff4f85bb98122f0190d41c0a64da
Juan Camilo <lozanojc@colorado.edu>
2012-09-18 22:37:58 -0600

    my goals for collin's and jay's class

--- goals.rb
+++ goals.rb
@@ -1,2 +1,3 @@
-goals.rb
+
 p "My goals for intro to programming are to be confident with Ruby and the terminal so that I can work efficiently.  I'm new to programming so I'm hoping this will be a great eye-opener into the wonderful world of Ruby. 
+
